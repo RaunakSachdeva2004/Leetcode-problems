86b1d3344940abeacc0ca90662b475675396fa79
Raunak Sachdeva <152476869+RaunakSachdeva2004@users.noreply.github.com>
2026-01-25 19:05:39 +0530

    Time: 2 ms (98.80%) | Memory: 44.2 MB (89.56%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,32 +1,26 @@
-// Using recursion
 class Solution {
     public boolean isPalindrome(String s) {
-        if( s == null || s.length() == 0)
+        int i = 0;
+        int j = s.length()-1;
+
+        while(i < j){
+            char left = s.charAt(i);
+            char right = s.charAt(j);
+
+            if(!Character.isLetterOrDigit(left)){
+                i++;
+                continue;
+            }
+            if(!Character.isLetterOrDigit(right)){
+                j--;
+                continue;
+            }
+            if(Character.toLowerCase(left) != Character.toLowerCase(right))
+                return false;
+
+            i++;
+            j--;
+        }
         return true;
-        return isPlaindromeRecursive(s, 0, s.length() -1 );
-    }
-
-    private boolean isPlaindromeRecursive(String s, int left, int right){
-        if(left >= right){
-            return true;
-        }
-
-        char leftchar = s.charAt(left);
-        char rightchar = s.charAt(right);
-
-        if(!Character.isLetterOrDigit(leftchar)){
-            return isPlaindromeRecursive(s, left + 1, right);
-        }
-
-        if(!Character.isLetterOrDigit(rightchar)){
-            return isPlaindromeRecursive(s, left, right -1);
-        }
-        
-
-        if(Character.toLowerCase(leftchar) != Character.toLowerCase(rightchar)){
-            return false;
-        }
-
-        return isPlaindromeRecursive(s, left+1, right-1);
     }
 }

3b87fabb4ef826c4e1bb54f087bc597d4fe89de1
Raunak Sachdeva <152476869+RaunakSachdeva2004@users.noreply.github.com>
2026-02-01 14:25:06 +0530

    Time: 7 ms (64.18%) | Memory: 44.1 MB (90.48%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,18 +1,50 @@
 class Solution {
     public String reverseWords(String s) {
-        String trim = s.trim();
+        int l = 0;
+        int r = s.length() - 1;
+        
+        // Trim leading spaces
+        while (l <= r && s.charAt(l) == ' ') l++;
+        // Trim trailing spaces
+        while (r >= l && s.charAt(r) == ' ') r--;
 
-        String[] arr= trim.split("\\s+");
-        int i = 0, j = arr.length-1;
-
-        while(i < j){
-            String temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-            i++;
-            j--;
+        StringBuilder sb = new StringBuilder();
+        // Remove internal multiple spaces
+        while (l <= r) {
+            char c = s.charAt(l);
+            if (c != ' ') {
+                sb.append(c);
+            } else if (sb.length() > 0 && sb.charAt(sb.length() - 1) != ' ') {
+                sb.append(' ');
+            }
+            l++;
         }
 
-        return String.join(" ", arr);
+        // Step 1: Reverse the whole string
+        reverse(sb, 0, sb.length() - 1);
+
+        // Step 2: Reverse each word back
+        int start = 0;
+        for (int end = 0; end < sb.length(); end++) {
+            // Find the end of the word
+            if (sb.charAt(end) == ' ') {
+                reverse(sb, start, end - 1);
+                start = end + 1;
+            } else if (end == sb.length() - 1) { // Last word
+                reverse(sb, start, end);
+            }
+        }
+
+        return sb.toString();
+    }
+
+    // Helper method to keep code clean
+    private void reverse(StringBuilder sb, int i, int j) {
+        while (i < j) {
+            char temp = sb.charAt(i);
+            sb.setCharAt(i, sb.charAt(j));
+            sb.setCharAt(j, temp);
+            i++; j--;
+        }
     }
 }

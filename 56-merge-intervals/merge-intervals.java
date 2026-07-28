1e1410dea5392266cd214752950a962693cb1394
Raunak Sachdeva <152476869+RaunakSachdeva2004@users.noreply.github.com>
2026-07-28 12:08:10 +0530

    Time: 9 ms (36.45%) | Memory: 48.9 MB (85.84%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,26 +1,30 @@
-
 class Solution {
     public int[][] merge(int[][] intervals) {
-        if (intervals.length <= 1) {
-            return intervals;
-        }
+        Arrays.sort(intervals, (a,b) -> Integer.compare(a[0],b[0]));
+        List<int[]> list = new ArrayList<>();
+        list.add(intervals[0]);
+        int n = intervals.length;
 
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        for(int i = 1; i< n;i++){
+            int[] prev = list.get(list.size()-1);
+            int[] curr = intervals[i];
 
-        List<int[]> merged = new ArrayList<>();
-        merged.add(intervals[0]);
-
-        for (int i = 1; i < intervals.length; i++) {
-            int[] currentInterval = intervals[i];
-            int[] lastMerged = merged.get(merged.size() - 1);
-
-            if (currentInterval[0] <= lastMerged[1]) {
-                lastMerged[1] = Math.max(lastMerged[1], currentInterval[1]);
-            } else {
-                merged.add(currentInterval);
+            if(curr[0] <= prev[1])
+            {
+                //merge
+                prev[0] = Math.min(prev[0], curr[0]);
+                prev[1] = Math.max(prev[1], curr[1]);
+            }
+            else{
+                list.add(intervals[i]);
             }
         }
 
-        return merged.toArray(new int[merged.size()][]);
+        int[][] res = new int[list.size()][2];
+        for(int i = 0; i< list.size(); i++){
+            res[i] = list.get(i);
+        }
+
+        return res;
     }
 }
